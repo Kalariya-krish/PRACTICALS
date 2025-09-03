fb8ba500220c62bd72f1c988ebe7960f848910ef
Kalariya Kris <kalariyakrish12@gmail.com>
2025-09-03 10:47:51 +0530

    Tutorial - 3 Completed

--- Lab_Tutorial_3/T3_Q6.cs
+++ Lab_Tutorial_3/T3_Q6.cs
@@ -4,5 +4,40 @@ namespace PRACTICALS.Lab_Tutorial_3
 {
     internal class T3_Q6
     {
+        class Line
+        {
+            private double length;
+            public Line()
+            {
+                Console.WriteLine("Object is being created, length = 10");
+                length = 10;
+            }
+
+            public void setLength(double len)
+            {
+                length = len;
+            }
+
+            public double getLength()
+            {
+                return length;
+            }
+        }
+
+        class TestLine
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Name : KALARIYA KRIS \nEnrollment No: 24SOECE13028\n");
+
+                Line line = new Line();
+
+                Console.WriteLine("Length of line : {0}", line.getLength());
+                line.setLength(6);
+
+                Console.WriteLine("Length of line : {0}", line.getLength());
+                Console.ReadKey();
+            }
+        }
     }
 }
--- Lab_Tutorial_3/T3_Q7.cs
+++ Lab_Tutorial_3/T3_Q7.cs
@@ -4,5 +4,66 @@ namespace PRACTICALS.Lab_Tutorial_3
 {
     internal class T3_Q7
     {
+        class Student
+        {
+            private string enrolmentNo;
+            private string name;
+
+            public string EnrolmentNo
+            {
+                get { return enrolmentNo; }
+                set { enrolmentNo = value; }
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public Student()
+            {
+                enrolmentNo = "N/A";
+                name = "Unknown";
+            }
+
+            public Student(string enrolmentNo, string name)
+            {
+                this.EnrolmentNo = enrolmentNo;
+                this.Name = name;
+            }
+
+            public void DisplayStudent()
+            {
+                Console.WriteLine($"Enrolment No: {EnrolmentNo}, Name: {Name}");
+            }
+        }
+
+        class TestStudent
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Name : KALARIYA KRIS \nEnrollment No: 24SOECE13028\n");
+
+                Student s1 = new Student();
+                Console.WriteLine("Student 1:");
+                s1.DisplayStudent();
+
+                Console.WriteLine();
+
+                Student s2 = new Student("24SOECE13028", "Kris");
+                Console.WriteLine("Student 2:");
+                s2.DisplayStudent();
+
+                Console.WriteLine();
+
+                s2.EnrolmentNo = "24SOECE99999";
+                s2.Name = "Updated Kris";
+                Console.WriteLine("Student 2 (After Update using Properties):");
+                s2.DisplayStudent();
+
+                Console.ReadKey();
+            }
+        }
     }
 }
